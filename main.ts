76bc8b3ae9faf40b0b0d00aae856aeac340087d0
Maheswaran M <git@maheswaranm.com>
2019-01-31 18:31:31 -0500

    basic drag and drop app to load and save back data from JSON file

--- main.ts
+++ main.ts
@@ -1,58 +1,56 @@
-import { app, BrowserWindow, screen } from 'electron';
+import { app, BrowserWindow, screen, ipcMain } from 'electron';
+import * as fs from 'fs';
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win
 
 const args = process.argv.slice(1);
 let serve = args.some(val => val === '--serve');
+let filepath = app.getPath('appData')+'/kanban.json'
 
 
 function createWindow () {
-  // Create the browser window.
   win = new BrowserWindow({ width: 800, height: 600 })
 
-  // and load the index.html of the app or the angular serve page if we are using serve.
   if (serve) {
     require('electron-reload')(__dirname, {
       electron: require(`../node_modules/electron`)
     });
     win.loadURL('http://localhost:4200');
+    win.webContents.openDevTools()
   } else {
     win.loadFile('./dist/index.html')
   } 
 
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
 })
+
+ipcMain.on('update', (event, arg) => {
+  fs.writeFile(filepath, JSON.stringify(arg), (err)=>{
+    if(err) throw err;
+  });
+
+  event.returnValue = 'done'
+});
+
+ipcMain.on('load', (event, arg) => {
+  console.log(arg);
+  event.returnValue = fs.readFileSync(filepath, 'utf8');
+});
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Card } from './classes/card';
+import { Board } from './classes/board';
+import { SortablejsOptions } from 'angular-sortablejs';
+import { ElectronService } from 'ngx-electron';
+
+
+import { MYBOARD, EMPTYBOARD } from './mocks/mock-board';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private _electronService: ElectronService) { 
+    this.board = JSON.parse(this._electronService.ipcRenderer.sendSync('load',123)); 
+  }
+
   title = 'personal-kanban-electron';
+  board = EMPTYBOARD;
+  
+
+  normalOptions: SortablejsOptions = {
+    group: 'normal-group',
+    onEnd: (event: any) => {
+      console.log('from ' + event.from.id + ' at ' + event.oldIndex + ' to ' + event.to.id + ' at ' + event.newIndex );
+
+      if(this._electronService.isElectronApp) {
+            let updateresult: string = this._electronService.ipcRenderer.sendSync('update',this.board);
+      }
+    }
+  };
+
 }
